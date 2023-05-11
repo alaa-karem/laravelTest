c18df27560cb5c76a264e6c93e5f33d800f66c7f
alaa <alaakarem182002@gmail.com>
2023-05-11 22:52:42 +0300

    add departments

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Department;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('departments', function(){
+    $departments = Department::get();
+    return view('departments',['departments'=>$departments]);
 });
